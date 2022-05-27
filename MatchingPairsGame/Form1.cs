5d9d06273218d5fa13e13ef4579027a91ae6ebe1
Jamar Arrington <jamar.arrington.git@gmail.com>
2022-05-27 15:21:39 -0500

    Test your memory by matching identical symbols

--- MatchingPairsGame/Form1.cs
+++ MatchingPairsGame/Form1.cs
@@ -12,14 +12,113 @@ namespace MatchingPairsGame
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null; // First label control that player clicks on /reference variable
+        Label secondClicked = null; // Second label control that player clicks on /reference variable
+
+        Random random = new Random(); // Creating random object
+        List<string> icons = new List<string>() // Creating list to hold icons
+        {
+            "!", "!", "@", "@", "#", "#", "$", "$",
+            "%", "%", "^", "^", "~", "~", "*", "*" 
+
+        };
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Event handler for clicking on icons
+        private void label_click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                // clickedLabel.ForeColor = Color.Black;
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+
+                }
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+
+
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop(); // Stop method, so timer doesn't run on startup
+            firstClicked.ForeColor = firstClicked.BackColor; // makes choice invisible
+            secondClicked.ForeColor = secondClicked.BackColor; // makes choice invisible
+            firstClicked = null;
+            secondClicked = null;
+
+        }
+
+        // Checking if player matched all icons
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("You matched all of the icons!", "Congratulations and well done!");
+            Close();
         }
     }
 }
